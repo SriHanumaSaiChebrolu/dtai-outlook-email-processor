2d63518f1de5cd7c221f996eabace43b03d009b0
SriHanumaSaiChebrolu <schebrolu@deltaintech.com>
2024-07-10 19:02:32 +0530

    Added code comments.

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const axios = require('axios');
 const AWS = require('aws-sdk');
 const qs = require('qs');
 
+// Fetches all unread emails from an outlook email
+// List of args
+// clientId - clientId captured from app registration in Azure with proper SMTP permissons
+// clientSecret - clientSecret captured from app registration in Azure with proper SMTP permissons
+// tenantId - tenantId of Azure account.
+// email - email id from which unread emails has to be read
+// response - Returns complete list of unread emails based on the above arguments
 async function fetchUnreadEmails(args) {
     const {
         clientId, clientSecret, tenantId, email
@@ -12,7 +19,7 @@ async function fetchUnreadEmails(args) {
         }
         else {
             const token = await generateToken(args);
-            const readEmailsUrl = `https://outlook.office365.com/api/v2.0/users/${email}/messages`
+            const readEmailsUrl = `https://outlook.office365.com/api/v2.0/users/${email}/messages?$filter=IsRead%20ne%20true`
             const readEmailsObj = {
                 method: 'get',
                 maxBodyLength: Infinity,
@@ -29,6 +36,13 @@ async function fetchUnreadEmails(args) {
     }
 }
 
+// Fetches all unread emails which has attachments from an outlook email
+// List of args
+// clientId - clientId captured from app registration in Azure with proper SMTP permissons
+// clientSecret - clientSecret captured from app registration in Azure with proper SMTP permissons
+// tenantId - tenantId of Azure account.
+// email - email id from which unread emails has to be read
+// response - Returns complete list of unread emails which has attachments based on the above arguments
 async function fetchUnreadEmailsWithOnlyAttachments(args) {
     const {
         clientId, clientSecret, tenantId, email
@@ -93,6 +107,14 @@ async function generateToken({ clientId, clientSecret, tenantId }) {
     }
 }
 
+// Fetches attachments based on messageId and returns base64 format of the attachment
+// List of args
+// clientId - clientId captured from app registration in Azure with proper SMTP permissons
+// clientSecret - clientSecret captured from app registration in Azure with proper SMTP permissons
+// tenantId - tenantId of Azure account.
+// email - email id from which unread emails has to be read
+// messageId = message id of the email for which attachments has to be read
+// response - Returns attachment in base64 along with filename and messageId
 async function readAttachmentUsingMessageId(args) {
     const {
         clientId, clientSecret, tenantId, email, messageId
@@ -128,6 +150,17 @@ async function readAttachmentUsingMessageId(args) {
     }
 }
 
+// Fetches attachments based on messageId, uploads to S3 bucket and marks email as read
+// List of args
+// clientId - clientId captured from app registration in Azure with proper SMTP permissons
+// clientSecret - clientSecret captured from app registration in Azure with proper SMTP permissons
+// tenantId - tenantId of Azure account.
+// email - email id from which unread emails has to be read
+// messageId = message id of the email for which attachments has to be read
+// awsaccessKeyId - aws access key of an user account which has permission to upload documents to S3.
+// awssecretAccessKey - aws access secret key of an user account which has permission to upload documents to S3.
+// awsBucket - aws bucket name to which documents has to be uploaded
+// response - Returns number of files uploaded to S3 bucket.
 async function readAndSaveAttachmentsToS3UsingMsgId(args) {
     const {
         clientId, clientSecret, tenantId, email, messageId, awsaccessKeyId, awssecretAccessKey, awsBucket
@@ -138,7 +171,9 @@ async function readAndSaveAttachmentsToS3UsingMsgId(args) {
         }
         else {
             const token = await generateToken(args);
-            return await getAttachmentsAndUploadAttachmentsToS3(email, messageId, awsaccessKeyId, awssecretAccessKey, awsBucket, token?.access_token);
+            const countOfAttachmentsUploaded = await getAttachmentsAndUploadAttachmentsToS3(email, messageId, awsaccessKeyId, awssecretAccessKey, awsBucket, token?.access_token);
+
+            return `${countOfAttachmentsUploaded} uploaded to provided S3 bucket`;
         }
     }
     catch (err) {
@@ -195,6 +230,16 @@ async function getAttachmentsAndUploadAttachmentsToS3(email, messageId, awsacces
     return uploadedAttachments;
 }
 
+// Fetches all attachments of unread emails, saves them to S3 bucket and marks email as read
+// List of args
+// clientId - clientId captured from app registration in Azure with proper SMTP permissons
+// clientSecret - clientSecret captured from app registration in Azure with proper SMTP permissons
+// tenantId - tenantId of Azure account.
+// email - email id from which unread emails has to be read
+// awsaccessKeyId - aws access key of an user account which has permission to upload documents to S3.
+// awssecretAccessKey - aws access secret key of an user account which has permission to upload documents to S3.
+// awsBucket - aws bucket name to which documents has to be uploaded
+// response - Returns number of files uploaded to S3 bucket.
 async function readUnReadEmailsAttachmentsSaveToS3(args) {
     const {
         clientId, clientSecret, tenantId, email, awsaccessKeyId, awssecretAccessKey, awsBucket
